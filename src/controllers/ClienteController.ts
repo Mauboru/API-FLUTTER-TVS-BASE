23e283a0ed697a7c6acd4f6bafe3357e00f51273
Josue Henrique <josue21henrique@gmail.com>
2024-03-21 13:20:43 -0300

    teste primario feito

--- src/controllers/ClienteController.ts
+++ src/controllers/ClienteController.ts
@@ -4,3 +4,18 @@ import { Cliente } from '../models/Cliente';
 import { Pedido } from '../models/Pedido';
 import { Op } from 'sequelize';
 
+export const GetClientById = async (req: Request, res: Response) => {
+    try{
+        const clienteId = parseInt(req.params.id, 10);
+        const cliente = await Cliente.findByPk(clienteId);
+
+        if (cliente){
+            res.json(cliente);
+        } else {
+            res.status(404).json({ message: 'Cliente não encontrado' });
+        }
+    } catch (error) {
+        console.error('Erro ao buscar cliente:', error);
+        res.status(500).json({ message: 'Erro ao buscar cliente'});
+    }
+};
---
+++ src/tests/TestClientController.spec.ts
@@ -0,0 +1,14 @@
+const request = require('supertest');
+import * as server from '../server';
+const app = server.server;
+import { Request, Response } from 'express';
+
+describe('Teste da rota GetClientById', () => {
+    it('deve retornar o cliente correto quando o ID é válido', async () => {
+        const clienteId = 1;
+        const response = await request(app).get(`/clientes/${clienteId}`);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('id', clienteId);
+    });
+})
